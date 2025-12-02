17738b75d11fb1f68eee914346236e9b004e4c84
Marten Klitzke <marten@klitzke.xyz>
2025-12-02 01:05:52 +0100

    chore(wxt): add initial setup files

---
+++ entrypoints/content.ts
@@ -0,0 +1,50 @@
+export default defineContentScript({
+  matches: [
+    "*://*.fleetyards.net/*",
+    "*://fleetyards.dev/*",
+    "*://fleetyards.test/*",
+  ],
+  main() {
+    function handleResponse(response: any) {
+      const origin = window.parent.location.origin;
+
+      if (origin === "https://fleetyards.dev") {
+        window.postMessage(
+          {
+            direction: "fy-sync",
+            message: response,
+          },
+          "https://fleetyards.dev"
+        );
+      } else if (origin === "https://fleetyards.net") {
+        window.postMessage(
+          {
+            direction: "fy-sync",
+            message: response,
+          },
+          "https://fleetyards.net"
+        );
+      } else if (origin === "http://fleetyards.test") {
+        window.postMessage(
+          {
+            direction: "fy-sync",
+            message: response,
+          },
+          "http://fleetyards.test"
+        );
+      }
+    }
+
+    window.addEventListener("message", async (event) => {
+      if (
+        event.source == window &&
+        event.data &&
+        event.data.direction == "fy"
+      ) {
+        const response = await browser.runtime.sendMessage(event.data.message);
+
+        handleResponse(response);
+      }
+    });
+  },
+});
---
+++ wxt.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig, version } from "wxt";
+
+// See https://wxt.dev/api/config.html
+export default defineConfig({
+  modules: ["@wxt-dev/module-vue"],
+  manifest: {
+    homepage_url: "https://fleetyards.net",
+    permissions: ["cookies", "https://robertsspaceindustries.com/*"],
+    host_permissions: ["https://robertsspaceindustries.com/*"],
+    icons: {
+      "16": "icon/16.png",
+      "32": "icon/32.png",
+      "48": "icon/48.png",
+      "96": "icon/96.png",
+      "128": "icon/128.png",
+    },
+  },
+});
